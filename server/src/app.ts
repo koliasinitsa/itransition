e24157747181339e45a39f58425878dcb5446c31
nikolay <koliasinitsa317@gmail.com>
2024-01-04 23:41:10 +0300

    servser commit

---
+++ server/src/app.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running ooodfgoo on http://localhost:${PORT}`);
+});
