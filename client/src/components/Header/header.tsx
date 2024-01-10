73d31d8190cc4bb2ed144597e111f0c78a42e316
nikolay <koliasinitsa317@gmail.com>
2024-01-10 22:58:17 +0300

    icon to home

--- client/src/components/Header/header.tsx
+++ client/src/components/Header/header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -9,6 +10,7 @@ import LanguageIcon from '@mui/icons-material/Language';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
 
 import SearchPanel from './Search-panel/search-panel';
 
@@ -36,10 +38,16 @@ const Header: React.FC = () => {
     return (
         <AppBar position="fixed">
             <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
-                {/* Поисковая строка (слева) с ограниченной шириной */}
-                <div style={{ maxWidth: '300px' }}>
-                    <SearchPanel />
+                <div style={{ display: 'flex', alignItems: 'center', }}>
+                    <Link to="/" style={{ marginRight: '100px' }}>
+                        <HomeIcon />
+                    </Link>
+                    {/* Поисковая строка (слева) с ограниченной шириной */}
+                    <div style={{ maxWidth: '300px' }}>
+                        <SearchPanel />
+                    </div>
                 </div>
+
 
                 {/* Иконка смены цвета и языковой выбор */}
                 <div>
@@ -69,6 +77,8 @@ const Header: React.FC = () => {
                         <MenuItem onClick={handleClose}>Eng</MenuItem>
                         <MenuItem onClick={handleClose}>Rus</MenuItem>
                     </Menu>
+
+
 
                     <Button color="inherit" style={{ marginLeft: '10px' }} onClick={handleLoginClick}>
                         Login
