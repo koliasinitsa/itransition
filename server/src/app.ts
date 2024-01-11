9403ef7fa1eeca3ca73214abb9de9c778dfdd0da
nikolay <koliasinitsa317@gmail.com>
2024-01-12 00:18:16 +0300

    schema bd server

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from 'express';
 const app = express();
 const PORT = 3000;
 
+
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with  Express!');
 });
