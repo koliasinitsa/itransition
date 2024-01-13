1ba3af930ae62cedaa87511e45694a666a25c0e4
nikolay <koliasinitsa317@gmail.com>
2024-01-13 22:29:38 +0300

    fix register server

--- server/src/Services/AuthService.ts
+++ server/src/Services/AuthService.ts
@@ -1,22 +1,28 @@
 //Логика для регистрации и авторизации пользователя.
 
 // src/services/authService.ts
-import { PrismaClient, UserStatus, UserRole } from '@prisma/client';
+import { PrismaClient, user_status, user_role } from '@prisma/client';
 import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
+//const saltRounds = 10;
 
 export const registerUserService = async (username: string, email: string, password: string) => {
   try {
+    // Генерация уникальной соли для каждого пользователя
+    //const salt = bcrypt.genSaltSync(saltRounds);
+
+    // Хеширование пароля с использованием уникальной соли
+    //const hashedPassword = await bcrypt.hash(password, salt);
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await prisma.user.create({
+    const user = await prisma.users.create({
       data: {
         username,
         email,
         password: hashedPassword,
-        status: UserStatus.active,
-        role: UserRole.user,
+        status: user_status.active,
+        role: user_role.user,
       },
     });
 
